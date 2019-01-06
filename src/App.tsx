057a2bc345e9cbba59d4350993b9b39a6f7b6999
Chris Boette <cboette@gmail.com>
2019-01-05 22:58:09 -0600

    Code cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import { Map } from './Map';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Map />
-      </div>
-    );
-  }
-}
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Map />
+  </div>
+);
 
 export default App;
--- src/Map.tsx
+++ src/Map.tsx
@@ -9,32 +9,45 @@ import { scaleLinear } from "d3-scale";
 
 import geo from "./static/world-50m-with-population.json";
 
+interface State {
+  countryName: string;
+}
+
+interface Props {}
+
 const wrapperStyles = {
   width: "100%",
-  maxWidth: 980,
+  maxWidth: "100%",
   margin: "0 auto",
 }
 
 const popScale = scaleLinear()
-  .domain([0,100000000,1400000000])
+  .domain([0, 100000000, 1400000000])
   .range(["#CFD8DC", "#607D8B", "#37474F"] as any[])
 
-export class Map extends Component {
+export class Map extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { countryName: "" } as State;
+  }
+
   handleClick = (geo: any) => {
-    console.log(geo.properties.formal_en);
+    const countryName = geo.properties.formal_en;
+    this.setState({ countryName });
   }
 
   render() {
     return (
       <div style={wrapperStyles}>
+        <div>{this.state.countryName}</div>
         <ComposableMap
-          projection="mercator"
+          projection="miller"
           projectionConfig={{
             scale: 175,
             rotation: [-11,0,0],
           }}
           width={1440}
-          height={899}
+          height={780}
           style={{
             width: "100%",
             height: "auto",
@@ -44,9 +57,9 @@ export class Map extends Component {
             <Geographies geography={geo}>
               {(geographies: any, projection: any) => geographies.map((geography: any, i: number) => (
                 <Geography
-                  key={ i }
-                  geography={ geography }
-                  projection={ projection }
+                  key={i}
+                  geography={geography}
+                  projection={projection}
                   onClick={this.handleClick}
                   style={{
                     default: {
@@ -56,6 +69,7 @@ export class Map extends Component {
                       outline: "none",
                     },
                     hover: {
+                      cursor: "pointer",
                       fill: "#263238",
                       stroke: "#607D8B",
                       strokeWidth: 0.75,
