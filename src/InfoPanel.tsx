2f506855d094ad0e59a7b7c59e75d9e792ea473f
Chris Boette <chrisbodhi@users.noreply.github.com>
2019-01-08 22:38:23 -0600

    [16 + 20] Add year slider; move population into panel (#21)
    * Install debounce for future use.
    
    * Create Slider for year selection. Move it and population into a panel.
    
    * Move shared styling into a single file. DRY code.

---
+++ src/InfoPanel.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { PopulationDisplay } from "./PopulationDisplay";
+import { Slider } from "./Slider";
+
+import "./InfoPanel.css";
+import "./panel.css";
+
+interface Props {
+  countryName: string;
+  isLoading: boolean;
+  onChange: (ev: React.FormEvent<HTMLInputElement>) => void;
+  population: string;
+  year: string;
+}
+
+export const InfoPanel = (props: Props) => {
+  const { countryName, population, isLoading, onChange, year } = props;
+  return countryName ?
+    (<div className="panel infoPanel">
+      <PopulationDisplay
+        countryName={countryName}
+        population={population}
+        isLoading={isLoading}
+      />
+      <Slider
+        onChange={onChange}
+        year={year}
+      />
+    </div>) :
+    null;
+}
+
+export default InfoPanel;
